dc4ba72fb42ae4ccd4a7229e490e80df00174171
Spencer Watson <wattyrev@gmail.com>
2024-11-13 19:58:02 -0800

    Restore passcode management with postgres

--- src/app/api/passcodes/route.ts
+++ src/app/api/passcodes/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
 
 /**
  * Change passcodes
@@ -28,26 +29,31 @@ export async function PUT(req: Request) {
         NextResponse.json({ message: 'No authentication provided'}, { status: 401 });
     }
 
-    const currentPasscode = 'admin'; //await kv.get('adminPasscode');
+    const fetchedPasscodes = await sql`SELECT passcode FROM passcodes WHERE name = 'admin'`;
+    if (!fetchedPasscodes.rows.length) {
+        throw new Error('Admin passcode not found');
+    }
+    const currentPasscode = fetchedPasscodes.rows[0].passcode;
+    
     if (authentication !== currentPasscode) {
         NextResponse.json({ message: 'Provided authentication is invalid'}, { status: 403 });
     }
 
-    // const promises = [];
+    const promises = [];
     const passcodesUpdated = []
     if (adminPasscode) {
-       // promises.push(kv.set('adminPasscode', adminPasscode));
-       passcodesUpdated.push('Admin');
+        promises.push(sql`UPDATE passcodes SET passcode = ${adminPasscode} WHERE name = 'admin'`);
+        passcodesUpdated.push('Admin');
     }
     if (editorPasscode) {
-       // promises.push(kv.set('editorPasscode', editorPasscode));
-       passcodesUpdated.push('Editor');
+        promises.push(sql`UPDATE passcodes SET passcode = ${editorPasscode} WHERE name = 'editor'`);
+        passcodesUpdated.push('Editor');
     }
     if (userPasscode) {
-       // promises.push(kv.set('userPasscode', userPasscode));
-       passcodesUpdated.push('User');
+        promises.push(sql`UPDATE passcodes SET passcode = ${userPasscode} WHERE name = 'user'`);
+        passcodesUpdated.push('User');
     }
-    // await Promise.all(promises);
+    await Promise.all(promises);
 
     return NextResponse.json({ message: `${passcodesUpdated.join(', ')} passcode${passcodesUpdated.length > 1 ? 's have' : ' has'} been updated`});
 }
@@ -74,11 +80,19 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'No passcode provided'}, { status: 400 });
     }
 
-    const editorPasscode = 'editor'; //await kv.get('editorPasscode');
+    const editorPasscodeResponse = await sql`SELECT passcode FROM passcodes WHERE name = 'editor'`;
+    if (!editorPasscodeResponse.rows.length) {
+        throw new Error('Editor passcode not found');
+    }
+    const editorPasscode = editorPasscodeResponse.rows[0].passcode;
     if (passcode === editorPasscode) {
         return NextResponse.json({ userType: 'editor' });
     }
-    const userPasscode = 'user'; //await kv.get('userPasscode');
+    const userPasscodeResponse = await sql`SELECT passcode FROM passcodes WHERE name = 'user'`;
+    if (!userPasscodeResponse.rows.length) {
+        throw new Error('Editor passcode not found');
+    }
+    const userPasscode = userPasscodeResponse.rows[0].passcode;
     if (passcode === userPasscode) {
        return NextResponse.json({ userType: 'user' });
     }
