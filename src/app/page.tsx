408d437f49c0ad4e26ee6edd3c630ca75dbc0d7c
Spencer Watson <wattyrev@gmail.com>
2024-11-15 21:05:10 -0800

    Fix types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import HomePageContainer from "@/components/HomePageContainer";
 import { sql } from "@vercel/postgres";
+import { Activity } from "./api/activities/route";
 
 export const revalidate = 10;
 export default async function Home() {
   const activitiesResponse = await sql`SELECT * FROM activities ORDER BY sortIndex ASC`;
-  const activities = activitiesResponse.rows.map(activity => {
-    const { id, title, description, sortIndex } = activity;
-    return { id, title, description, sortIndex };
+  const activities: Activity[] = activitiesResponse.rows.map(activity => {
+    const { id, title, description, sortindex, scheduleindex } = activity;
+    return { id, title, description, sortIndex: sortindex, scheduleIndex: scheduleindex };
   });
   console.log('fetched')
  
