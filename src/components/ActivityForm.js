5b2177df04d54dd5efda4ceafa13140aa6258e50
Spencer Watson <wattyrev@gmail.com>
2025-01-13 20:59:43 -0800

    rename shit

---
+++ src/components/ActivityForm.js
@@ -0,0 +1,44 @@
+'use client'
+
+import { useState } from "react";
+import { useAuth } from "./global/Auth";
+import Panel from "./ui/Panel";
+import TextInput from "./ui/TextInput";
+import Textarea from "./ui/Textarea";
+import Button from "./ui/Button";
+import H1 from "./ui/H1";
+import { css } from "../../styled-system/css";
+
+const ActivityForm = ({ onSubmit, title: defaultTitle = '', description: defaultDescription = '', onCancel, autoFocus = false }) => {
+    const { isEditor } = useAuth();
+    const [ pending, setPending ] = useState(false);
+    const [ title, setTitle ] = useState(defaultTitle);
+    const [ description, setDescription ] = useState(defaultDescription);
+
+    if (!isEditor()) {
+        return null;
+    }
+
+    async function submit() {
+        setPending(true);
+        await onSubmit({ title, description });
+        setTitle(defaultTitle);
+        setDescription(defaultDescription);
+        setPending(false);
+    }
+    return (
+        <form data-testid="create-activity-form" onSubmit={e => {e.preventDefault(); submit()}}>
+            <H1 className={css({ paddingLeft: '16px', paddingTop: '32px'})}>Create Activity</H1>
+            <Panel>
+                <label>Title</label>
+                <TextInput autoFocus={autoFocus} data-testid="title" required value={title} onChange={e => setTitle(e.target.value)} />
+                <label>Description</label>
+                <Textarea data-testid="description" required value={description} onChange={e => setDescription(e.target.value)} />
+                <Button data-testid="save-activity" type="submit" disabled={pending}>Save</Button>
+                {onCancel && <Button data-testid="cancel-activity" className="secondary" onClick={onCancel}>Cancel</Button>}
+            </Panel>
+        </form>
+    )
+}
+
+export default ActivityForm;
---
+++ src/components/__tests__/ActivityForm.test.js
@@ -0,0 +1,64 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { useAuth } from '../global/Auth';
+import ActivityForm from '../ActivityForm';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('../global/Auth');
+
+describe('ActivityForm', () => {
+    it('does not render for non-editors', async () => {
+        useAuth.mockReturnValue({
+            isEditor: jest.fn().mockReturnValue(false)
+        });
+        render(<ActivityForm />);
+
+        expect(screen.queryByTestId('create-activity')).not.toBeInTheDocument();
+    });
+    it('allows an editor to create an activity', async () => {
+        useAuth.mockReturnValue({
+            isEditor: jest.fn().mockReturnValue(true)
+        });
+        const onSubmit = jest.fn().mockResolvedValue();
+        render(<ActivityForm onSubmit={onSubmit} />);
+
+        await userEvent.type(screen.getByTestId('title'), 'Cool Activity');
+        await userEvent.type(screen.getByTestId('description'), 'This is a cool activity');
+        await userEvent.click(screen.getByTestId('save-activity'));
+
+        expect(onSubmit).toHaveBeenCalledWith({ title: 'Cool Activity', description: 'This is a cool activity' });
+    });
+    it('disables the save button while pending', async () => {
+        useAuth.mockReturnValue({
+            isEditor: jest.fn().mockReturnValue(true)
+        });
+        let resolveSubmit;
+        const onSubmit = jest.fn().mockImplementation(() => {
+            return new Promise(resolve => {
+                resolveSubmit = resolve;
+            })
+        });
+        render(<ActivityForm onSubmit={onSubmit} />);
+
+        await userEvent.type(screen.getByTestId('title'), 'Cool Activity');
+        await userEvent.type(screen.getByTestId('description'), 'This is a cool activity');
+        await userEvent.click(screen.getByTestId('save-activity'));
+
+        expect(screen.getByTestId('save-activity')).toHaveAttribute('disabled');
+        resolveSubmit();
+        await waitFor(() => expect(screen.getByTestId('save-activity')).not.toHaveAttribute('disabled'));
+    });
+    it('empties out the form after saving', async () => {
+        useAuth.mockReturnValue({
+            isEditor: jest.fn().mockReturnValue(true)
+        });
+        const onSubmit = jest.fn().mockResolvedValue()
+        render(<ActivityForm onSubmit={onSubmit} />);
+
+        await userEvent.type(screen.getByTestId('title'), 'Cool Activity');
+        await userEvent.type(screen.getByTestId('description'), 'This is a cool activity');
+        await userEvent.click(screen.getByTestId('save-activity'));
+
+        expect(screen.getByTestId('title')).toHaveValue('');
+        expect(screen.getByTestId('description')).toHaveValue('');
+    })
+})
