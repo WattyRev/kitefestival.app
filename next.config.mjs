c24361e1cf1e52043d50722dfb22365a5a40ce27
Spencer Watson <wattyrev@gmail.com>
2026-06-11 15:22:18 -0700

    Fix issue where events cannot be created when events is empty (#56)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,         // Check for changes every second
+        aggregateTimeout: 300, // Delay the rebuild slightly
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
